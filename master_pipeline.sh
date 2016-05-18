25e3ea730e7beda6e831de219ee07ec677a37eda
Yahiaoui Rachid <rac021@hotmail.com>
2016-05-18 23:22:02 +0200

    Update master_pipeline.sh

--- master_pipeline.sh
+++ master_pipeline.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-
+if [ $# -eq 2 ] 
+then
+    
 . ./scripts/01_infra_build.sh  $1
 
 . ./scripts/02_infra_deploy.sh $2
+
+else
+    echo "invalid argument please pass only two arguments "
+fi
 
