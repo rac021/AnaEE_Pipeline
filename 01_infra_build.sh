87213f8564c306559ed6f78eaaa6746017a32dc3
Yahiaoui Rachid <rac021@hotmail.com>
2016-05-18 18:01:46 +0200

    Update 01_infra_build.sh
    To be able to set environment variables, this script must be run by :  . ./01_infra_build.sh

--- 01_infra_build.sh
+++ 01_infra_build.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-docker rmi -f $(docker images)
+DEFAULT_BLZ_IMAGE="blazegraph"
 
-docker build -t blazegraph .
+  if test ! -z "$1" ; then
+   DEFAULT_BLZ_IMAGE=$1
+  fi
+       
+if docker history -q $DEFAULT_BLZ_IMAGE >/dev/null 2>&1; then
+    echo "$DEFAULT_BLZ_IMAGE already exist, remove it..."
+    docker rmi -f $DEFAULT_BLZ_IMAGE
+fi
+
+export BLZ_IMAGE=$DEFAULT_BLZ_IMAGE
+
+echo "building image : $DEFAULT_BLZ_IMAGE "
+
+docker build -t $BLZ_IMAGE Docker
