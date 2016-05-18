8fc9476daffaf3e7a24e1f99ae89e93556b9da81
Yahiaoui Rachid <rac021@hotmail.com>
2016-05-18 16:00:17 +0200

    Update 02_infra_deploy.sh

--- 02_infra_deploy.sh
+++ 02_infra_deploy.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-
 # Docker version min : 1.10 
-docker network create --subnet=192.168.56.250/24 mynet123
+
+# check if subnet exist, create it else 
+subnet=`docker network ls | grep mynet123`
+# docker network rm mynet123
+if [[ "${subnet}" == *mynet123* ]]; then
+  echo "subnet already exsting ";
+else
+  echo " create subnet mynet123 "
+  docker network create --subnet=192.168.56.250/24 mynet123
+fi
 
 docker run -d --net mynet123 --name host_2 \
        --add-host host_0:192.168.56.100    \
-       --add-host host_1:192.168.56.101    \ 
+       --add-host host_1:192.168.56.101    \
        --add-host host_2:192.168.56.102    \
        --ip 192.168.56.102 -p 7777:9999    \
        --expose 9999 -it --entrypoint /bin/bash blazegraph -c "./bigdata start ; while true; do sleep 1000; done "
@@ -23,6 +31,9 @@ docker run -d --net mynet123 --name host_0 \
        --add-host host_2:192.168.56.102    \
        --ip 192.168.56.100 -p 9999:9999    \
        -it --entrypoint /bin/bash blazegraph -c "./bigdata start; while true; do sleep 1000; done "
-       
+
+echo "waiting for cluter... "
+sleep(10)   
+
 # Run bigdata cluster using host_2 as EndPoint
 docker exec host_2 ./nanoSparqlServer.sh 9999 ola 
