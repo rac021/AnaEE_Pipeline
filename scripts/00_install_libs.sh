b827d48609c39d2e32e74bb64932e6a49ea3ff00
Yahiaoui Rachid <rac021@hotmail.com>
2016-05-19 22:24:59 +0200

    Update 00_install_libs.sh

--- scripts/00_install_libs.sh
+++ scripts/00_install_libs.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
 
-rootPath=`pwd`
-directoryInstall="libs"
-tmp="tmp"
+echo "Executing Script : $0 "
+echo ""
 
-if [ -d "$directoryInstall" ] ; then
-mkdir $directoryInstall
+CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CURRENT_DIRECTORY="scripts"
+ROOT_PATH="${CURRENT_PATH/'/'$CURRENT_DIRECTORY/''}"
+
+DIRECTORY_LIBS="libs"
+DIRECTORY_DATA="data"
+DIRECTORY_DATA_ONTOP="ontop"
+DIRECTORY_DATA_CORESE="corese"
+DIRECTORY_DATA_CONFIG="conf"
+TMP="tmp"
+
+if [ ! -d "$ROOT_PATH/$DIRECTORY_LIBS" ]; then
+cd $ROOT_PATH/$DIRECTORY_LIBS
+echo "create FOLER :      $ROOT_PATH/$DIRECTORY_LIBS "
+mkdir -p $ROOT_PATH/$DIRECTORY_LIBS
+fi
+if [ ! -d "$ROOT_PATH/$DIRECTORY_DATA" ]; then
+mkdir -p $ROOT_PATH/$DIRECTORY_DATA
+fi
+if [ ! -d "$ROOT_PATH/$DIRECTORY_DATA/$DIRECTORY_DATA_ONTOP" ]; then
+mkdir $ROOT_PATH/$DIRECTORY_DATA/$DIRECTORY_DATA_ONTOP
+fi
+if [ ! -d "$ROOT_PATH/$DIRECTORY_DATA/$DIRECTORY_DATA_CORESE" ]; then
+mkdir -p $ROOT_PATH/$DIRECTORY_DATA/$DIRECTORY_DATA_CORESE
+fi
+if [ ! -d "$ROOT_PATH/$CURRENT_DIRECTORY/$DIRECTORY_DATA_CONFIG" ]; then
+mkdir -p $ROOT_PATH/$CURRENT_DIRECTORY/$DIRECTORY_DATA_CONFIG
 fi
 
 #######################
@@ -21,14 +45,13 @@ echo ""
 sleep 2
 tput setaf 7
 
-
-git clone https://github.com/rac021/obdaYedGen-3.14.2.git $directoryInstall/$tmp
-cd $rootPath/$directoryInstall/$tmp
+git clone https://github.com/rac021/obdaYedGen-3.14.2.git $DIRECTORY_LIBS/$TMP
+cd $ROOT_PATH/$DIRECTORY_LIBS/$TMP
 mvn clean install assembly:single 
-mv $rootPath/$directoryInstall/$tmp/target/YedODBA-3.14.2-1.0-SNAPSHOT-jar-with-dependencies.jar \
-   $rootPath/$directoryInstall/YedODBA.jar
+mv $ROOT_PATH/$DIRECTORY_LIBS/$TMP/target/YedODBA-3.14.2-1.0-SNAPSHOT-jar-with-dependencies.jar \
+   $ROOT_PATH/$DIRECTORY_LIBS/YedODBA.jar
 
-rm -rf $rootPath/$directoryInstall/$tmp/* $rootPath/$directoryInstall/$tmp/.git
+rm -rf $ROOT_PATH/$DIRECTORY_LIBS/$TMP/* $ROOT_PATH/$DIRECTORY_LIBS/$TMP/.git
 
 ##################################
 ### Install Ontop-Materializer ###
@@ -43,13 +66,13 @@ echo ""
 sleep 2
 tput setaf 7
 
-git clone https://github.com/rac021/ontop-matarializer.git $rootPath/$directoryInstall/$tmp
-cd $rootPath/$directoryInstall/$tmp
+git clone https://github.com/rac021/ontop-matarializer.git $ROOT_PATH/$DIRECTORY_LIBS/$TMP
+cd $ROOT_PATH/$DIRECTORY_LIBS/$TMP
 mvn clean install assembly:single
-mv $rootPath/$directoryInstall/$tmp/target/ontop-materializer-1.17.0-jar-with-dependencies.jar \
-   $rootPath/$directoryInstall/ontop-materializer.jar
+mv $ROOT_PATH$DIRECTORY_LIBS/$TMP/target/ontop-materializer-1.17.0-jar-with-dependencies.jar \
+   $ROOT_PATH/$directoryInstall/ontop-materializer.jar
 
-rm -rf $rootPath/$directoryInstall/$tmp/* $rootPath/$directoryInstall/$tmp/.git
+rm -rf $ROOT_PATH/$DIRECTORY_LIBS/$TMP/* $ROOT_PATH/$DIRECTORY_LIBS/$TMP/.git
 
 
 ##################################
@@ -65,20 +88,20 @@ echo ""
 sleep 2
 tput setaf 7
 
-git clone https://github.com/rac021/CoreseInfer.git $rootPath/$directoryInstall/$tmp
-cd $rootPath/$directoryInstall/$tmp
+git clone https://github.com/rac021/CoreseInfer.git $ROOT_PATH/$DIRECTORY_LIBS/$TMP
+cd $ROOT_PATH/$DIRECTORY_LIBS/$TMP
 mvn clean install assembly:single
-mv $rootPath/$directoryInstall/$tmp/target/CoreseInferMaven-1.0.0-jar-with-dependencies.jar \
-   $rootPath/$directoryInstall/CoreseInfer.jar
+mv $ROOT_PATH/$DIRECTORY_LIBS/$TMP/target/CoreseInferMaven-1.0.0-jar-with-dependencies.jar \
+   $ROOT_PATH/$DIRECTORY_LIBS/CoreseInfer.jar
 
-rm -rf $rootPath/$directoryInstall/$tmp/* $rootPath/$directoryInstall/$tmp/.git
+rm -rf $ROOT_PATH/$DIRECTORY_LIBS/$TMP/* $ROOT_PATH/$DIRECTORY_LIBS/$TMP/.git
 
 
 #########################
-#### Clean tmp folder ###
+#### Clean TMP folder ###
 #########################
 
-rm -rf $rootPath/$directoryInstall/$tmp/
+rm -rf $ROOT_PATH/$DIRECTORY_LIBS/$TMP/
 
 tput setaf 2
 echo ""
