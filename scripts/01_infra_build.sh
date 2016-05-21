d139dbe3d9a6197e51e8838b02e638966ba91267
Yahiaoui Rachid <rac021@hotmail.com>
2016-05-21 22:20:26 +0200

    Update 01_infra_build.sh

--- scripts/01_infra_build.sh
+++ scripts/01_infra_build.sh
@@ -1,44 +1,41 @@
 #!/bin/bash
 
-# Note :
-# To be able to set environment variables, 
-# this script mut be launched using :  . ./01_infra_build.sh
+if [ $# -eq 1 ] ; then
 
-CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+	# Docker Image
+	DOCKER_BLZ_IMAGE=$1
 
-# Default name of docker image
-DEFAULT_BLZ_IMAGE="BLZ"
+	CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-DOCKER_FILE_PATH="$CURRENT_PATH/Docker"
-
-if [ ! -e $DOCKER_FILE_PATH ]
-
-then
-    echo "$CURRENT_PATH Not found !! Haa Project cloned from Git ? "
-else
-       	if test ! -z "$1" ; then
-	   DEFAULT_BLZ_IMAGE=$1
-	fi
-       
-	if docker history -q $DEFAULT_BLZ_IMAGE >/dev/null 2>&1; then
-
-	    CONTAINER_ID=`docker images -q $DEFAULT_BLZ_IMAGE `
-	    echo "$DEFAULT_BLZ_IMAGE already exist, remove it..."
-	    echo "Conainer ID : $CONTAINER_ID "
-	    docker rmi -f $CONTAINER_ID
-	fi
-
-	export BLZ_IMAGE=$DEFAULT_BLZ_IMAGE
-
-	tput setaf 2
-	echo ""
-	echo "############################################"
-	echo "### Building Image : $DEFAULT_BLZ_IMAGE  ###"
-	echo "############################################"
-	echo ""
-	sleep 2
-	tput setaf 7
+	DOCKER_FILE_PATH="$CURRENT_PATH/Docker"
 	
-	docker build -t $DEFAULT_BLZ_IMAGE $DOCKER_FILE_PATH
+	if [ ! -e $DOCKER_FILE_PATH ]; then
+	
+	    echo "$CURRENT_PATH Not found !! Haa Project cloned from Git ? "
+	
+	else
+		if docker history -q $DOCKER_BLZ_IMAGE >/dev/null 2>&1 ; then
+	
+		    CONTAINER_ID=`docker images -q $DOCKER_BLZ_IMAGE `
+		    echo "$DOCKER_BLZ_IMAGE already exist, remove it..."
+		    echo "Conainer ID : $CONTAINER_ID "
+		    docker rmi -f $CONTAINER_ID
+		fi
+		
+		tput setaf 2
+		echo ""
+		echo "############################################"
+		echo "### Building Image : $DOCKER_BLZ_IMAGE   ###"
+		echo "############################################"
+		echo ""
+		sleep 2
+		tput setaf 7
+		
+		docker build -t $DOCKER_BLZ_IMAGE $DOCKER_FILE_PATH
+	fi
+
+else
+    echo " Invalid argument : please pass exactly One arguments "
+    echo " arg_1 : Image_Docker_Name                            "
 fi
 
