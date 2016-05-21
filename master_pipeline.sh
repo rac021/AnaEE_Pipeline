f61db8a97211fa0bd661291357fc6079e3f4abfa
Yahiaoui Rachid <rac021@hotmail.com>
2016-05-21 22:08:26 +0200

    Update master_pipeline.sh

--- master_pipeline.sh
+++ master_pipeline.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-if [ $# -eq 2 ] 
+if [ $# -eq 3 ] 
 then
 
   ./scripts/00_install_libs.sh 
 
 . ./scripts/01_infra_build.sh  $1
 
-. ./scripts/02_infra_deploy.sh $2
+. ./scripts/02_infra_deploy.sh $1 $2 $3 
 
 else
-    echo "invalid argument : please pass exactly two arguments "
-    echo " arg_1 : Image_docker_name                           "
-    echo " arg_2 : Blazegraph_namespace                        "
+    echo " Invalid argument : please pass exactly three arguments "
+    echo " arg_1 : Image_docker_name                              "
+    echo " arg_2 : Blazegraph_namespace                           "
+    echo " arg_3 : Ports  number                                  "
     
 fi
 
