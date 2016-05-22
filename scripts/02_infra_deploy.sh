c918e99bce6114d2882e315baa84832b5b910534
Yahiaoui Rachid <rac021@hotmail.com>
2016-05-22 16:40:29 +0200

    Update 02_infra_deploy.sh

--- scripts/02_infra_deploy.sh
+++ scripts/02_infra_deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Note :
+# Do not touch HOST_NAME
 # Docker version min : 1.10 
 # $1 blazegraph nameSpace 
 # $2 NameSpace
@@ -9,20 +10,28 @@
 
 if [ $# -eq 7 ] ; then
 
+	# Cluster Host Name. Do not TOUCH 
+	HOST_0="blz_host_0"
+	HOST_1="blz_host_1"
+	HOST_2="blz_host_2"
+	
+	# IP per Host # Configurable
+	IP_HOST_0="192.168.56.200"
+	IP_HOST_1="192.168.56.201"
+	IP_HOST_2="192.168.56.202"
+		
 	# Get Image Docker Name
 	BLZ_IMAGE=$1
 	# Get NameSpace
 	NAMESPACE=$2
 	# Get Port Number
 	PORT=$3
-	# Cluster 
-	HOST_0=$4
-	HOST_1=$5
-	HOST_2=$6
 	# Get Default Mode : 
 	# 'rw' for read-write Mode
 	# 'ro' for readOnly Mode. 
-	DEFAULT_MODE=$7
+	DEFAULT_MODE=$4
+	
+	LOOP="while true; do sleep 1000; done"
 	
 	if [ "$DEFAULT_MODE" != "ro" ] && [ "$DEFAULT_MODE" != "rw" ] ; then 
 	echo "DEFAULT_MODE can only have 'rw' OR 'ro' values !!"
@@ -53,7 +62,6 @@ if [ $# -eq 7 ] ; then
 	sleep 2
 	tput setaf 7
 
-
 	SUBNET_CHECK=`docker network ls | grep $SUBNET`
 	
 	if docker history -q $BLZ_IMAGE >/dev/null 2>&1; then
@@ -62,55 +70,56 @@ if [ $# -eq 7 ] ; then
 	      echo "subnet - $SUBNET - already exists "
 	    else
 	      echo " create subnet $SUBNET "
-	      docker network create --subnet=192.168.56.250/24 $SUBNET # docker network rm $SUBNET
+	      docker network create --subnet=192.168.56.250/24 $SUBNET 
+	      # docker network rm $SUBNET
 	    fi
 	
             EXIST=`docker inspect --format='{{.Name}}' $( docker ps -aq --no-trunc) | grep $HOST_0`
 	    if [ ! -z $EXIST ]; then 
 	      echo "Container $HOST_0 already exists, remove..."
-	      docker rm -f $HOST_0
+	      docker  rm  -f  $HOST_0
 	      echo "Container $HOST_0 removed !!"
 	    fi
 	    
             EXIST=`docker inspect --format='{{.Name}}' $( docker ps -aq --no-trunc) | grep $HOST_1`
 	    if [ ! -z $EXIST ]; then 
 	      echo "Container $HOST_1 already exists, remove..."	    
-	      docker rm -f $HOST_1
+	      docker  rm  -f  $HOST_1
 	      echo "Container $HOST_1 removed !!"	      
 	    fi
 	    
             EXIST=`docker inspect --format='{{.Name}}' $( docker ps -aq --no-trunc) | grep $HOST_2`
 	    if [ ! -z $EXIST ]; then 
 	      echo "Container $HOST_2 already exists, remove..."
-	      docker rm -f $HOST_2
+	      docker  rm  -f  $HOST_2
 	      echo "Container $HOST_2 removed !!"
 	    fi
 	
 	    docker run -d --net mynet123 --name $HOST_2 \
-	           --add-host host_0:192.168.56.100     \
-	           --add-host host_1:192.168.56.101     \
-	           --add-host host_2:192.168.56.102     \
-	           --ip 192.168.56.102 -p 7777:9999     \
+	           --add-host $HOST_0:$IP_HOST_0        \
+	           --add-host $HOST_1:$IP_HOST_1        \
+	           --add-host $HOST_2:$IP_HOST_2        \
+	           --ip $IP_HOST_2   -p   7777:9999     \
 	           --expose 9999                        \
-	           -it --entrypoint /bin/bash $BLZ_IMAGE -c "./bigdata start ; while true; do sleep 1000; done "
+	           -it --entrypoint /bin/bash $BLZ_IMAGE -c "./bigdata start ;$LOOP "
 	
 	    sleep 4
 	
 	    docker run -d --net mynet123 --name $HOST_1 \
-	           --add-host host_0:192.168.56.100     \
-	           --add-host host_1:192.168.56.101     \
-	           --add-host host_2:192.168.56.102     \
-	           --ip 192.168.56.101 -p 8888:9999     \
-	           -it --entrypoint /bin/bash $BLZ_IMAGE -c "./bigdata start; while true; do sleep 1000; done "
+	           --add-host $HOST_0:$IP_HOST_0        \
+	           --add-host $HOST_1:$IP_HOST_1        \
+	           --add-host $HOST_2:$IP_HOST_2        \
+	           --ip $IP_HOST_1   -p   8888:9999     \
+	           -it --entrypoint /bin/bash $BLZ_IMAGE -c "./bigdata start; $LOOP "
 	
 	    sleep 4 
 	
 	    docker run -d --net mynet123 --name $HOST_0 \
-	           --add-host host_0:192.168.56.100     \
-	           --add-host host_1:192.168.56.101     \
-	           --add-host host_2:192.168.56.102     \
-	           --ip 192.168.56.100 -p 9999:9999     \
-	           -it --entrypoint /bin/bash $BLZ_IMAGE -c "./bigdata start; while true; do sleep 1000; done "
+	           --add-host $HOST_0:$IP_HOST_0        \
+	           --add-host $HOST_1:$IP_HOST_1        \
+	           --add-host $HOST_2:$IP_HOST_2        \
+	           --ip $IP_HOST_0  -p    9999:9999     \
+	           -it --entrypoint /bin/bash $BLZ_IMAGE -c "./bigdata start; $LOOP "
 	
 	    echo "waiting for blazegraph Cluster... "
 	    
@@ -129,10 +138,7 @@ else
   echo " arg_1             : Image_docker_name                          "
   echo " arg_2             : Blazegraph_namespace                       "
   echo " arg_3             : Ports  number                              "
-  echo " arg_4             : Container Name One                         "
-  echo " arg_5             : Container Name Two                         "
-  echo " arg_6             : Container Name Three                       "
-  echo " arg_7             : Mode : rw ( read-wrire) ;  ro ( readOnly ) "
+  echo " arg_4             : Mode : rw ( read-wrire) ;  ro ( readOnly ) "
     
 fi
 
