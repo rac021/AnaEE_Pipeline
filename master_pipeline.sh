30716fcabb890f35dce8b7d5d14ab97acde3fcd8
Yahiaoui Rachid <rac021@hotmail.com>
2016-05-22 02:46:09 +0200

    Update master_pipeline.sh

--- master_pipeline.sh
+++ master_pipeline.sh
@@ -12,13 +12,13 @@ then
   ./scripts/02_infra_deploy.sh $1 $2 $3 $4 $5 $6 rw
 
 else
-    echo " Invalid argument : please pass exactly Six arguments   "
-    echo " arg_1            : Image_docker_name                   "
-    echo " arg_2            : Blazegraph_namespace                "
-    echo " arg_3            : Ports  number                       "
-    echo " arg_4            : Container Name One                  "
-    echo " arg_5            : Container Name Two                  "
-    echo " arg_6            : Container Name Three                "
+    echo " Invalid arguments :  please pass exactly Six arguments   "
+    echo " arg_1             :  Image_docker_name                   "
+    echo " arg_2             :  Blazegraph_namespace                "
+    echo " arg_3             :  Ports  number                       "
+    echo " arg_4             :  Container Name One                  "
+    echo " arg_5             :  Container Name Two                  "
+    echo " arg_6             :  Container Name Three                "
     
 fi
 
