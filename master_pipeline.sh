4c51e76f86d971203043889dd7fe580f452c7bf8
Yahiaoui Rachid <rac021@hotmail.com>
2016-05-22 17:02:08 +0200

    Update master_pipeline.sh

--- master_pipeline.sh
+++ master_pipeline.sh
@@ -16,7 +16,7 @@ if [ $# -eq 6 ] ; then
 
 else
 
-    echo " Invalid arguments :  please pass exactly Six arguments   "
+    echo " Invalid arguments :  please pass exactly Seven arguments "
     echo " arg_1             :  Image_docker_name                   "
     echo " arg_2             :  Blazegraph_namespace                "
     echo " arg_3             :  Ports  number                       "
