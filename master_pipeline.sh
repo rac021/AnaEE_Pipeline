7da52ae804412c317e8814d7ab008b097f817c73
Yahiaoui Rachid <rac021@hotmail.com>
2016-05-22 02:45:44 +0200

    Update master_pipeline.sh

--- master_pipeline.sh
+++ master_pipeline.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-if [ $# -eq 3 ] 
+if [ $# -eq 6 ] 
 then
 
    chmod +x scripts/*
      
   ./scripts/00_install_libs.sh 
 
-  ./scripts/01_infra_build.sh  $1
+  ./scripts/01_infra_build.sh  $1 $4 $5 $6
 
-  ./scripts/02_infra_deploy.sh $1 $2 $3 rw
+  ./scripts/02_infra_deploy.sh $1 $2 $3 $4 $5 $6 rw
 
 else
-    echo " Invalid argument : please pass exactly three arguments "
-    echo " arg_1 : Image_docker_name                              "
-    echo " arg_2 : Blazegraph_namespace                           "
-    echo " arg_3 : Ports  number                                  "
+    echo " Invalid argument : please pass exactly Six arguments   "
+    echo " arg_1            : Image_docker_name                   "
+    echo " arg_2            : Blazegraph_namespace                "
+    echo " arg_3            : Ports  number                       "
+    echo " arg_4            : Container Name One                  "
+    echo " arg_5            : Container Name Two                  "
+    echo " arg_6            : Container Name Three                "
     
 fi
 
