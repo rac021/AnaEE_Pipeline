b21262c7fe7337cb377b76b76dce4de71514f96f
Yahiaoui Rachid <rac021@hotmail.com>
2016-05-25 22:49:12 +0200

    Update 04_gen_mapping.sh

--- scripts/04_gen_mapping.sh
+++ scripts/04_gen_mapping.sh
@@ -5,21 +5,21 @@
   EXTENSION=".graphml"
    
   tput setaf 2
-	echo 
-	echo " ######################################### "
-	echo " ######## Info Generation ################ "
-	echo " ----------------------------------------- "
-	echo
-	echo " ##  Script    : $0                        " 
-	echo " ##  INPUT     : $INPUT                    "
-	echo " ##  EXTENTION : $EXTENSION                "
+  echo 
+  echo " ######################################### "
+  echo " ######## Info Generation ################ "
+  echo " ----------------------------------------- "
   echo
-	echo " ##  OUTPUT    : $OUTPUT                   "
-	echo
-	echo " ######################################### "
-	echo 
-	sleep 1
-	tput setaf 7
+  echo " ##  Script    : $0                        " 
+  echo " ##  INPUT     : $INPUT                    "
+  echo " ##  EXTENTION : $EXTENSION                "
+  echo
+  echo " ##  OUTPUT    : $OUTPUT                   "
+  echo
+  echo " ######################################### "
+  echo 
+  sleep 1
+  tput setaf 7
 	
   echo -e "\e[90m Strating Generation... \e[39m "
         
