9acabb09f464db453dcaaab83bdbf045089578cf
Yahiaoui Rachid <rac021@hotmail.com>
2016-05-25 20:10:37 +0200

    Update 06_corese_infer.sh

--- scripts/06_corese_infer.sh
+++ scripts/06_corese_infer.sh
@@ -2,21 +2,27 @@
     
   OWL="../Mapping/ontology.owl"
   TTL="../data/ontop/ontopMaterializedTriples.ttl"
-  QUERY=" SELECT ?S ?P ?O { ?S ?P ?O } "
+  QUERY="select ?S ?P ?O { ?S ?P ?O } "
   OUTPUT="../data/corese"
-    
+  f="100000"
+  F="n3"
+  ilt="t"
+  
   tput setaf 2
   echo 
   echo " ######################################### "
   echo " ######## Info Generation ################ "
   echo " ----------------------------------------- "
   echo
-  echo " ##  Script : $0                           " 
+  echo " ##  Script   : $0                         " 
   echo
-  echo " ##  OWL    : $OWL                         "
-  echo " ##  TTL    : $TTL                         "
-  echo " ##  QUERY  : $QUERY                       "
-  echo " ##  OUTPUT : $OUTPUT                      "
+  echo " ##  OWL      : $OWL                       "
+  echo " ##  TTL      : $TTL                       "
+  echo " ##  QUERY    : $QUERY                     "
+  echo " ##  OUTPUT   : $OUTPUT                    "
+  echo " ##  FORMAT   : $F                         "
+  echo " ##  FRAGMENT : $f                         "
+  
   echo
   echo " ######################################### "
   echo 
@@ -27,14 +33,14 @@
   echo
 
   java -Xms1024M -Xmx2048M -cp ../libs/CoreseInfer.jar corese.Main \
-  -owl $OWL                                                        \
-  -nt  $TTL                                                        \
-  -q   $QUERY                                                      \
-  -out $OUTPUT                                                     \
-  -f   100000                                                      \
-  -ilv t                                                           \
-  -F   n3 
-      
+  -owl "$OWL"                                                      \
+  -nt  "$TTL"                                                      \
+  -q   "$QUERY"                                                    \
+  -out "$OUTPUT"                                                   \
+  -f   "$f"                                                        \
+  -ilv "$ilt"                                                      \
+  -F   "$F" 
+   
   echo -e "\e[36m Triples Generated in : $OUTPUT \e[39m "
   echo
         
