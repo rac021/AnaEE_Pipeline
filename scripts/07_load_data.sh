521467a03e4d672cb7178f6c0e7dc1311efa0e24
Yahiaoui Rachid <rac021@hotmail.com>
2016-05-25 23:43:43 +0200

    Update 07_load_data.sh

--- scripts/07_load_data.sh
+++ scripts/07_load_data.sh
@@ -8,9 +8,16 @@
     if [ -f $NANO_END_POINT_FILE ] ; then
     
         FIRST_END_POINT=$(head -n 1 $NANO_END_POINT_FILE )
-        echo "--> $FIRST_END_POINT "
+        
+        IFS=’:’ read -ra INFO_NANO <<< "$FIRST_END_POINT" 
+        NANO_END_POINT_HOST=${INFO_NANO[0]}
+        NANO_END_POINT_IP=${INFO_NANO[1]}
+        NANO_END_POINT_PORT=${INFO_NANO[2]}
+        NAME_SPACE=${INFO_NANO[3]}
+        
         
         else 
+        
         echo " Oupss, config missed ! "
     fi
     
