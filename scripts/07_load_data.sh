eae1a9f4c36cfeede68984625c26734a392f8929
Yahiaoui Rachid <rac021@hotmail.com>
2016-05-25 23:58:16 +0200

    Update 07_load_data.sh

--- scripts/07_load_data.sh
+++ scripts/07_load_data.sh
@@ -1,25 +1,38 @@
 #!/bin/bash
 
     CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-    
     NANO_END_POINT_FILE="$CURRENT_PATH/conf/nanoEndpoint"
-
+    DATA_DIR="../data/corese"
     
     if [ -f $NANO_END_POINT_FILE ] ; then
     
-        FIRST_END_POINT=$(head -n 1 $NANO_END_POINT_FILE )
+      FIRST_END_POINT=$(head -n 1 $NANO_END_POINT_FILE )
+      
+      IFS=’:’ read -ra INFO_NANO <<< "$FIRST_END_POINT" 
         
-        IFS=’:’ read -ra INFO_NANO <<< "$FIRST_END_POINT" 
-        NANO_END_POINT_HOST=${INFO_NANO[0]}
-        NANO_END_POINT_IP=${INFO_NANO[1]}
-        NANO_END_POINT_PORT=${INFO_NANO[2]}
-        NAME_SPACE=${INFO_NANO[3]}
+      NANO_END_POINT_IP=${INFO_NANO[1]}
+      NANO_END_POINT_PORT=${INFO_NANO[2]}
         
+      ENDPOINT="http://$NANO_END_POINT_IP:$NANO_END_POINT_PORT/bigdata/sparql"
+
+      echo " endpoint ==> $ENDPOINT"
+       
+      sleep 1  # Waits 1 second.
+
+        
+      cd $DATA_DIR
+       
+      for FILE in `ls -a *.*`
+         do
+            echo "---------------------"
+            echo "Upload file : $FILE" 
+            echo "---------------------"
+            curl -D- -H 'Content-Type: text/turtle' --upload-file $FILE -X POST $ENDPOINT -O
+            echo "---------------------"
+        done
         
         else 
         
-        echo " Oupss, config missed ! "
+           echo " Oupss, config missed ! "
     fi
-    
-
     
