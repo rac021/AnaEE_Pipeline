ad6aad095106bd914b4babe45e8aca37a7060382
Yahiaoui Rachid <rac021@hotmail.com>
2016-05-25 17:45:22 +0200

    Create 05_ontop_gen_triples.sh

---
+++ scripts/05_ontop_gen_triples.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+    
+  OWL="../Mapping/ontology.owl"
+  OBDA="../Mapping/mapping.obda"
+  OUTPUT="../data/ontop/ontopMaterializedTriples.ttl"
+  QUERY="SELECT ?S ?P ?O { ?S ?P ?O } "
+  TTL="-ttl"
+    
+  tput setaf 2
+	echo 
+	echo " ######################################### "
+	echo " ######## Info Generation ################ "
+	echo " ----------------------------------------- "
+	echo
+	echo " ##  Script : $0                           " 
+	echo
+	echo " ##  OWL    : $OWL                         "
+	echo " ##  OBDA   : $OBDA                        "
+  echo " ##  OUTPUT : $OUTPUT                      "
+  echo " ##  QUERY  : $QUERY                       "
+  echo " ##  TTL    : $TTL                         "
+	echo
+	echo " ######################################### "
+	echo 
+	sleep 1
+	tput setaf 7
+	
+  echo -e "\e[90m Strating Generation... \e[39m "
+  echo
+        
+  java  -Xms1024M -Xmx2048M -cp ../libs/Ontop-Materializer.jar ontop.Main_1_17 \
+  -owl  $OWL                                                                   \
+  -obda $OBDA                                                                  \
+  -out  $OUTPUT                                                                \
+  -q    $QUERY                                                                 \
+  $TTL
+     
+  echo -e "\e[36m Triples Generated in : $OUTPUT \e[39m "
+  echo
+        
+        
