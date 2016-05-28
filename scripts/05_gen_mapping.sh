b8431d0164d8ec6d5c83e529ec1066e23f43270e
Yahiaoui Rachid <rac021@hotmail.com>
2016-05-28 23:07:42 +0200

    Update 05_gen_mapping.sh

--- scripts/05_gen_mapping.sh
+++ scripts/05_gen_mapping.sh
@@ -20,16 +20,16 @@
   echo 
   sleep 1
   tput setaf 7
-	
+
+  cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
   if [ ! -d $INPUT ] ; then
      echo -e "\e[91m $INPUT is not a valid Directory ! \e[39m "
      exit 3
   fi
 
   echo -e "\e[90m Strating Generation... \e[39m "
-
-  cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
+  
   java -cp ../libs/yedGen.jar Main -d $INPUT -out $OUTPUT -ext $EXTENSION
 
   echo -e "\e[36m Mapping generated in : $OUTPUT \e[39m "
