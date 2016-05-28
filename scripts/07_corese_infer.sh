f2b75e17085a62ac2b68ee17680f7f36e6e2680d
Yahiaoui Rachid <rac021@hotmail.com>
2016-05-28 22:02:13 +0200

    Update 07_corese_infer.sh

--- scripts/07_corese_infer.sh
+++ scripts/07_corese_infer.sh
@@ -28,7 +28,13 @@
   echo 
   sleep 1
   tput setaf 7
-	
+
+
+  if [ ! -f $OWL ]  || [ ! -f $TTL ]  ; then
+     echo -e "\e[91m Missing OWL or TTL Files ! \e[39m "
+     exit 3
+  fi
+  
   echo -e "\e[90m Strating Generation... \e[39m "
   echo
 
