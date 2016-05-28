4a1b74be4f33aaabc64773a826935fb8723849d6
Yahiaoui Rachid <rac021@hotmail.com>
2016-05-28 15:58:24 +0200

    Update 09_query_example.sh

--- scripts/09_query_example.sh
+++ scripts/09_query_example.sh
@@ -37,12 +37,13 @@
   	
     curl -X POST $ENDPOINT --data-urlencode \
     'query=PREFIX : <http://www.anaee/fr/soere/ola#> 
-     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-     PREFIX oboe-core: <http://ecoinformatics.org/oboe/oboe.1.0/oboe-core.owl#>
-      SELECT ?uri ?unite {
-     	 ?uri a oboe-core:Measurement .
-   	   ?uri oboe-core:hasValue ?unite
-    }
+           PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+           PREFIX oboe-core: <http://ecoinformatics.org/oboe/oboe.1.0/oboe-core.owl#>
+           SELECT ?uri ?unite {
+     	        ?uri a oboe-core:Measurement .
+   	          ?uri oboe-core:hasValue ?unite
+           }
+           LIMIT 2
    ' \
    -H 'Accept:application/json'
    
