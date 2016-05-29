5d48dec23513079b61223d60c887ec587156a280
Yahiaoui Rachid <rac021@hotmail.com>
2016-05-29 21:16:34 +0200

    Update 10_query_example.sh

--- scripts/10_query_example.sh
+++ scripts/10_query_example.sh
@@ -37,8 +37,7 @@
   	tput setaf 7
   	
     curl -X POST $ENDPOINT --data-urlencode \
-    'query=PREFIX : <http://www.anaee/fr/soere/ola#> 
-           PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    'query=PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
            PREFIX oboe-core: <http://ecoinformatics.org/oboe/oboe.1.0/oboe-core.owl#>
            SELECT ?uri ?unite {
      	        ?uri a oboe-core:Measurement .
