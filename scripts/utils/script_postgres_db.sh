737bef3bd8587c1015cfe7d08448ef8c9f1397e9
Yahiaoui Rachid <rac021@hotmail.com>
2016-06-01 20:23:40 +0200

    Update script_postgres_db.sh

--- scripts/utils/script_postgres_db.sh
+++ scripts/utils/script_postgres_db.sh
@@ -51,16 +51,16 @@
   
   );
 
-  INSERT INTO physicochimiebysitevariableyear VALUES ('s_code_1', 's_name_1', 'dtype_code_1', 'dtype_name_1', 'var_code_1', 'var_name_1', 'uni_id_1', 'uni_code_1','uni_name_1', '06-09-2015', 69) ;
-  INSERT INTO physicochimiebysitevariableyear VALUES ('s_code_2', 's_name_2', 'dtype_code_2', 'dtype_name_2', 'var_code_2', 'var_name_2', 'uni_id_2', 'uni_code_2','uni_name_1', '06-09-2015', 81) ;
-  INSERT INTO physicochimiebysitevariableyear VALUES ('s_code_3', 's_name_3', 'dtype_code_3', 'dtype_name_3', 'var_code_3', 'var_name_3', 'uni_id_3', 'uni_code_3','uni_name_1', '06-09-2015', 75) ;
-  INSERT INTO physicochimiebysitevariableyear VALUES ('s_code_4', 's_name_4', 'dtype_code_4', 'dtype_name_4', 'var_code_4', 'var_name_4', 'uni_id_4', 'uni_code_4','uni_name_4', '06-09-2015', 20) ;
-  INSERT INTO physicochimiebysitevariableyear VALUES ('s_code_5', 's_name_5', 'dtype_code_5', 'dtype_name_5', 'var_code_5', 'var_name_5', 'uni_id_5', 'uni_code_5','uni_name_5', '06-09-2015', 56) ;
-  INSERT INTO physicochimiebysitevariableyear VALUES ('s_code_6', 's_name_6', 'dtype_code_6', 'dtype_name_6', 'var_code_6', 'var_name_6', 'uni_id_6', 'uni_code_6','uni_name_6', '06-09-2015', 64) ;
-  INSERT INTO physicochimiebysitevariableyear VALUES ('s_code_7', 's_name_7', 'dtype_code_7', 'dtype_name_7', 'var_code_7', 'var_name_7', 'uni_id_7', 'uni_code_7','uni_name_7', '06-09-2015', 94) ;
-  INSERT INTO physicochimiebysitevariableyear VALUES ('s_code_8', 's_name_8', 'dtype_code_8', 'dtype_name_8', 'var_code_8', 'var_name_8', 'uni_id_8', 'uni_code_8','uni_name_8', '06-09-2015', 20) ;
-  INSERT INTO physicochimiebysitevariableyear VALUES ('s_code_9', 's_name_9', 'dtype_code_9', 'dtype_name_9', 'var_code_9', 'var_name_9', 'uni_id_9', 'uni_code_9','uni_name_9', '06-09-2015', 21) ;
-  INSERT INTO physicochimiebysitevariableyear VALUES ('s_code_10', 's_name_10', 'dtype_code_10', 'dtype_name_10', 'var_code_10', 'var_name_10', 'uni_id_10', 'uni_code_10','uni_name_10', '06-09-2015', 69) ;
+  INSERT INTO physicochimiebysitevariableyear VALUES ('s_code_01', 's_name_01', 'dtype_code_01', 'dtype_name_01', 'var_code_01', 'var_name_01', 'uni_id_01', 'uni_code_01','uni_name_01', '06-09-2001', 21) ;
+  INSERT INTO physicochimiebysitevariableyear VALUES ('s_code_02', 's_name_02', 'dtype_code_02', 'dtype_name_02', 'var_code_02', 'var_name_02', 'uni_id_02', 'uni_code_02','uni_name_02', '06-09-2002', 81) ;
+  INSERT INTO physicochimiebysitevariableyear VALUES ('s_code_03', 's_name_03', 'dtype_code_03', 'dtype_name_03', 'var_code_03', 'var_name_03', 'uni_id_03', 'uni_code_03','uni_name_03', '06-09-2003', 75) ;
+  INSERT INTO physicochimiebysitevariableyear VALUES ('s_code_04', 's_name_04', 'dtype_code_04', 'dtype_name_04', 'var_code_04', 'var_name_04', 'uni_id_04', 'uni_code_04','uni_name_04', '06-09-2004', 20) ;
+  INSERT INTO physicochimiebysitevariableyear VALUES ('s_code_05', 's_name_05', 'dtype_code_05', 'dtype_name_05', 'var_code_05', 'var_name_05', 'uni_id_05', 'uni_code_05','uni_name_05', '06-09-2005', 56) ;
+  INSERT INTO physicochimiebysitevariableyear VALUES ('s_code_06', 's_name_06', 'dtype_code_06', 'dtype_name_06', 'var_code_06', 'var_name_06', 'uni_id_06', 'uni_code_06','uni_name_06', '06-09-2006', 64) ;
+  INSERT INTO physicochimiebysitevariableyear VALUES ('s_code_07', 's_name_07', 'dtype_code_07', 'dtype_name_07', 'var_code_07', 'var_name_07', 'uni_id_07', 'uni_code_07','uni_name_07', '06-09-2007', 94) ;
+  INSERT INTO physicochimiebysitevariableyear VALUES ('s_code_08', 's_name_08', 'dtype_code_08', 'dtype_name_08', 'var_code_08', 'var_name_08', 'uni_id_08', 'uni_code_08','uni_name_08', '06-09-2008', 20) ;
+  INSERT INTO physicochimiebysitevariableyear VALUES ('s_code_09', 's_name_09', 'dtype_code_09', 'dtype_name_09', 'var_code_09', 'var_name_09', 'uni_id_09', 'uni_code_09','uni_name_09', '06-09-2009', 21) ;
+  INSERT INTO physicochimiebysitevariableyear VALUES ('s_code_10', 's_name_10', 'dtype_code_10', 'dtype_name_10', 'var_code_10', 'var_name_10', 'uni_id_10', 'uni_code_10','uni_name_10', '06-09-2010', 69) ;
 
   GRANT SELECT ON $TABLE to $USER ;	
 
