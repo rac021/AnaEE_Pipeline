a18ad95463a8b9f03187f7ee78d1c5c857bb3065
Yahiaoui Rachid <rac021@hotmail.com>
2016-06-08 18:12:52 +0200

    Update Queries_mapping_p_phimie.sql

--- scripts/utils/Queries_mapping_p_phimie.sql
+++ scripts/utils/Queries_mapping_p_phimie.sql
@@ -20,18 +20,19 @@ Measurement (6)
 -----------------------------
 
 SELECT
-	dt.code AS dty_code ,
-    var.code AS variable_code,
-	public.chimiesynthesisvalue.id AS value_id,
-	public.chimiesynthesisvalue.valuefloat AS value,	
+	dt.code AS dty_code,
+	var.code AS variable_code,
+	public.valeur_mesure_chimie_vmchimie.valeur,
+	public.valeur_mesure_chimie_vmchimie.vmchimie_id
 FROM
 	public.datatype dt
 	INNER JOIN public.datatypevariableuniteglacpe dtvu
 	 ON dt.id = dtvu.dty_id
 	INNER JOIN public.variable_glacpe_varg var
 	 ON dtvu.var_id = var.id
-	INNER JOIN public.chimiesynthesisvalue
-	 ON var.nom = public.chimiesynthesisvalue.variable
+	INNER JOIN public.valeur_mesure_chimie_vmchimie
+	 ON var.id = public.valeur_mesure_chimie_vmchimie.var_id
 WHERE
 	dt.id = 11
 	and var.nom = 'Azote Ammoniacal'
+	
