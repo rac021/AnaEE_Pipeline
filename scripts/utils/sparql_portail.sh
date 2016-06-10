d8f58efccd44778e709ebcee31c26859a7e00dca
Yahiaoui Rachid <rac021@hotmail.com>
2016-06-10 13:12:05 +0200

    Update sparql_portail.sh

--- scripts/utils/sparql_portail.sh
+++ scripts/utils/sparql_portail.sh
@@ -13,3 +13,55 @@ SELECT ?varStandardName ?valueNitro WHERE {
   
          
 }
+
+
+
+
+
+
+
+
+CONSTRUCT 
+{  ?nameSite a :An...
+   ?anneedipso a :Yead ...
+   ?count a Total..
+   ?target :mentionCount ?mentionCount .
+} 
+WHERE
+{  SELECT (COUNT(?mention) AS ?mentionCount) ?target
+   {  ?mention :mentionOf ?target .
+   } GROUP BY ?target
+}
+
+
+
+
+
+
+
+
+
+PREFIX : <http://www.anaee-france.fr/ontology/anaee-france_ontology#> 
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+PREFIX oboe-core: <http://ecoinformatics.org/oboe/oboe.1.0/oboe-core.owl#> 
+PREFIX oboe-standard: <http://ecoinformatics.org/oboe/oboe.1.0/oboe-core.owl#Standard>
+
+
+SELECT ?categName  ?unite WHERE  {
+  
+    ?obs_var_1 a oboe-core:Observation ; oboe-core:ofEntity :Variable ; oboe-core:hasContext ?obs_categ_03 , ?obs_var_05 .
+  
+    ?obs_categ_03  a oboe-core:Observation ; oboe-core:ofEntity :VariableCategory ; oboe-core:hasMeasurement ?measu_categName_4 .
+    ?measu_categName_4 a oboe-core:Measurement ; oboe-core:usesStandard :Anaee-franceVariableCategoryNamingStandard ;
+                         oboe-core:hasValue ?categName .
+  
+    ?obs_var_05  a oboe-core:Observation ; oboe-core:ofEntity  ?notVariableCateg ;  oboe-core:hasMeasurement ?measu_unit_06 .
+    NOT EXISTS { ?obs_var_05 a :VariableCategory . }
+        
+    ?measu_unit_06 a oboe-core:Measurement ; oboe-core:hasValue ?unite .
+      
+  
+     
+      
+  
+}
