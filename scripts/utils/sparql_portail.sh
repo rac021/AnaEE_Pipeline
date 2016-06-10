f8e7a212bbaa8280154b922a1366f3cee90e7e0f
Yahiaoui Rachid <rac021@hotmail.com>
2016-06-10 21:38:00 +0200

    Update sparql_portail.sh

--- scripts/utils/sparql_portail.sh
+++ scripts/utils/sparql_portail.sh
@@ -1,3 +1,65 @@
+PREFIX : <http://www.anaee-france.fr/ontology/anaee-france_ontology#> 
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+PREFIX oboe-core: <http://ecoinformatics.org/oboe/oboe.1.0/oboe-core.owl#> 
+PREFIX oboe-standard: <http://ecoinformatics.org/oboe/oboe.1.0/oboe-standards.owl#>
+PREFIX oboe-temporal: <http://ecoinformatics.org/oboe/oboe.1.0/oboe-temporal.owl#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+SELECT ?site ?categName ?variable ?unite ?date (COUNT(?variable) as ?nbVariable) WHERE {
+  
+?obs_var_1 a oboe-core:Observation ; oboe-core:ofEntity :Variable ; oboe-core:hasMeasurement ?measu_variable_02 ; 
+             oboe-core:hasContext ?obs_categ_03 , ?obs_var_05 .
+  
+?measu_variable_02 a oboe-core:Measurement ; oboe-core:usesStandard :Anaee-franceVariableNamingStandard ; oboe-core:hasValue ?variable .
+  
+?obs_categ_03 a oboe-core:Observation ; oboe-core:ofEntity :VariableCategory ; 
+                oboe-core:hasMeasurement ?measu_categName_4 .  
+
+  
+?measu_categName_4 a oboe-core:Measurement ; oboe-core:usesStandard :Anaee-franceVariableCategoryNamingStandard ;
+oboe-core:hasValue ?categ  .
+  
+  ?categ rdfs:label ?categName .
+  
+  
+?obs_var_05 a oboe-core:Observation ; oboe-core:ofEntity :Nitrogen ; oboe-core:hasMeasurement ?measu_unit_06 ;
+              oboe-core:hasContext+   ?obs_timeinstant_13 , ?obs_exPlot_15 .
+              
+ 
+?measu_unit_06 a oboe-core:Measurement ; oboe-core:usesStandard ?unite .
+  
+    ?obs_timeinstant_13 a oboe-core:Observation ; oboe-core:ofEntity oboe-temporal:TimeInstant ; oboe-core:hasMeasurement ?measu_date_15 .
+     ?measu_date_15 a oboe-core:Measurement ; oboe-core:hasValue ?date .
+  
+  
+    ?obs_exPlot_15 a oboe-core:Observation ; oboe-core:ofEntity :ExperimentalPlot ; oboe-core:hasContext ?obs_site_17 .
+  
+   ?obs_site_17 a oboe-core:Observation ; oboe-core:ofEntity :ExperimentalSite ; oboe-core:hasMeasurement ?meas_siteName_18 .
+  
+     ?meas_siteName_18 a oboe-core:Measurement ; oboe-core:usesStandard :Anaee-franceExperimentalSiteNamingStandard ;oboe-core:hasValue ?site .
+  
+}
+
+
+group by ?site ?categName ?variable ?unite ?date
+
+
+***************************************************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 PREFIX : <http://www.anaee-france.fr/ontology/anaee-france_ontology#> 
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
