8801ec40926b007b3121d9c3fe15d622f1d1fb2f
Yahiaoui Rachid <rac021@hotmail.com>
2016-06-20 09:32:02 +0200

    Update 09_load_data_single_node.sh

--- scripts/09_load_data_single_node.sh
+++ scripts/09_load_data_single_node.sh
@@ -57,64 +57,63 @@
            
       done
         
-        RES=`curl -s -I $ENDPOINT | grep HTTP/1.1 | awk {'print $2'}`
+      RES=`curl -s -I $ENDPOINT | grep HTTP/1.1 | awk {'print $2'}`
         
-        COUNT=0
+      COUNT=0
         
-         while  [ -z $RES ] || [ $RES -ne 200 ] ;do
+      while  [ -z $RES ] || [ $RES -ne 200 ] ;do
         
-          sleep 1
-          RES=`curl -s -I $ENDPOINT | grep HTTP/1.1 | awk {'print $2'}`
-          let "COUNT++" 
-           
-          if  [ -z $RES ] || [ $RES -ne 200 ] ; then 
-              if [ `expr $COUNT % 3` -eq 0 ] ; then
-                 echo -e " attempt to join $ENDPOINT .. "
-              fi
-          fi
-           
-        done
-       
-        echo " Yeah Connected !! "
-        
-        if [ ! -d $DATA_DIR ] ; then
-           echo -e "\e[91m $DATA_DIR is not valid Directory ! \e[39m "
-           exit 3
-        fi
-            
-        # Remove the sparql file automatically created by blazegraph
-        if [ -f "$DATA_DIR/sparql" ] ; then
-          rm -f "$DATA_DIR/sparql"
-        fi
-
-        sleep 0.5  # Waits 0.5 second.
-
-        cd $DATA_DIR
-        
-        tput setaf 2
-        echo 
-        echo -e " ######################################################## "
-        echo -e " ######## Info Load Data ################################ "
-        echo -e " -------------------------------------------------------- "
-        echo -e " \e[90m$0       \e[32m                                    "
-        echo
-        echo -e " # ENDPOINT : $ENDPOINT                                   "
-        echo
-        echo -e " ######################################################## "
-        echo 
         sleep 1
-        tput setaf 7
+        RES=`curl -s -I $ENDPOINT | grep HTTP/1.1 | awk {'print $2'}`
+        let "COUNT++" 
+           
+        if  [ -z $RES ] || [ $RES -ne 200 ] ; then 
+            if [ `expr $COUNT % 3` -eq 0 ] ; then
+               echo -e " attempt to join $ENDPOINT .. "
+            fi
+        fi
+           
+      done
+       
+      echo " Yeah Connected !! "
         
-        for FILE in `ls -a *.*`
-          do
-            echo " ---------------------                                                   "
-            echo -e " \e[39m Upload file :\e[92m $DATA_DIR/$FILE \e[39m                    " 
-            echo " ----------------------------------------------------------------------- "
-            echo
-            curl -D- -H 'Content-Type: text/turtle' --upload-file $FILE -X POST $ENDPOINT -O
-            echo
-            echo " ----------------------------------------------------------------------- "
-        done
+      if [ ! -d $DATA_DIR ] ; then
+         echo -e "\e[91m $DATA_DIR is not valid Directory ! \e[39m "
+         exit 3
+      fi
+            
+      # Remove the sparql file automatically created by blazegraph
+      if [ -f "$DATA_DIR/sparql" ] ; then
+        rm -f "$DATA_DIR/sparql"
+      fi
+
+      sleep 0.5  # Waits 0.5 second.
+
+      cd $DATA_DIR
+        
+      tput setaf 2
+      echo 
+      echo -e " ######################################################## "
+      echo -e " ######## Info Load Data ################################ "
+      echo -e " -------------------------------------------------------- "
+      echo -e " \e[90m$0       \e[32m                                    "
+      echo
+      echo -e " # ENDPOINT : $ENDPOINT                                   "
+      echo
+      echo -e " ######################################################## "
+      echo 
+      sleep 1
+      tput setaf 7
+        
+      for FILE in `ls -a *.*`
+        do
+          echo " ---------------------                                                   "
+          echo -e " \e[39m Upload file :\e[92m $DATA_DIR/$FILE \e[39m                    " 
+          echo " ----------------------------------------------------------------------- "
+          echo
+          curl -D- -H 'Content-Type: text/turtle' --upload-file $FILE -X POST $ENDPOINT -O
+          echo
+          echo " ----------------------------------------------------------------------- "
+      done
         
    
-    
