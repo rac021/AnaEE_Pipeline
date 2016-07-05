beb24bbba009a089c3b6d1f1cd25e2d7ca6695b9
Yahiaoui Rachid <rac021@hotmail.com>
2016-07-05 12:33:46 +0200

    Update 00_install_libs.sh

--- scripts/00_install_libs.sh
+++ scripts/00_install_libs.sh
@@ -153,11 +153,13 @@ echo " ################################## "
 echo " ### Install Ontop-Materializer ### "
 echo " ################################## "
 echo "                                    "
+
 if [ "$DATA_BASE" != "" ] ; then 
   echo -e "\e[90m    Database : $DATA_BASE  \e[32m "
 else
   echo -e "\e[90m    Database : porstgreSQL \e[32m "
-  
+fi
+
 echo
 
 sleep 2
