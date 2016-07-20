60f32150fcaad4105bfad78685e68eb74a8a45a6
Yahiaoui Rachid <rac021@hotmail.com>
2016-07-20 21:36:25 +0200

    Update 11_listServices.sh

--- scripts/11_listServices.sh
+++ scripts/11_listServices.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+  EXIT() {
+    parent_script=`ps -ocommand= -p $PPID | awk -F/ '{print $NF}' | awk '{print $1}'`
+    if [ $parent_script = "bash" ] ; then
+        exit 2
+    else
+        kill -9 `ps --pid $$ -oppid=`;
+        exit 2
+    fi
+  }
+  
   checkIfContainersAreRunning() {
      
      for LINE in `cat $1`; do
@@ -12,14 +22,14 @@
          echo
          echo -e "\e[91m UNKNOWN - Container $CONTAINER does not exist. \e[37m "
          echo
-         exit 3
+         EXIT
        fi
 
        if [ "$RUNNING" == "false" ]; then
          echo
          echo -e "\e[91m CRITICAL - Container $CONTAINER is not running. \e[37m "
          echo
-         exit 2
+         EXIT
        fi
      done
   } 
