62be54eb186af2a5aa5257e80c2570f77be2e1c3
Yahiaoui Rachid <rac021@hotmail.com>
2016-08-24 16:22:22 +0200

    Update 08_ontop_gen_triples.sh

--- scripts/08_ontop_gen_triples.sh
+++ scripts/08_ontop_gen_triples.sh
@@ -10,13 +10,15 @@
   XMX=${7:-"-Xmx2048M"}
 
   EXIT() {
-    parent_script=`ps -ocommand= -p $PPID | awk -F/ '{print $NF}' | awk '{print $1}'`
-    if [ $parent_script = "bash" ] ; then
-        exit 2
-    else
-        kill -9 `ps --pid $$ -oppid=`;
-        exit 2
-    fi
+     parent_script=`ps -ocommand= -p $PPID | awk -F/ '{print $NF}' | awk '{print $1}'`
+     if [ $parent_script = "bash" ] ; then
+         echo; echo -e " \e[90m exited by : $0 \e[39m " ; echo
+         exit 2
+     else
+         echo ; echo -e " \e[90m exited by : $0 \e[39m " ; echo
+         kill -9 `ps --pid $$ -oppid=`;
+         exit 2
+     fi
   }
   
   tput setaf 2
